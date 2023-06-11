baa2fa47021266401eb6c383cc585d3fc49e8f38
Rayahna-Ghabris <10120555@mu.edu.lb>
2023-06-11 10:37:36 +0300

    Add project files.

---
+++ MUSM-api-MVCwebapp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MUSM_api_MVCwebapp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
